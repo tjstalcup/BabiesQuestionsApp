59fce3392a7acfed523090b4900de02c692728c7
SCallahan101 <Shannonc619@gmail.com>
2018-12-23 11:57:36 -0700

    add CRUD process

--- appModel.js
+++ appModel.js
@@ -1,16 +1,19 @@
 "use strict"
 
 // const mongoose = require('mongoose');
+const uuid = require('uuid/v4');
 
 const childQuestionSchema = mongoose.Schema({
   parentName: {type: String, required: true},
   // Need more idea
   zipcode: {type: String},
+  title: {type: String, required: true},
   question: {
     date: Date,
-    text: String,
+    content: String,
     childAge: Number,
     foundAnswer: String
   },
+  id: uuid
 // save later
 })
--- originalServer.js
+++ originalServer.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.static('FrontMain'));
 
+// function testpage() {
+//   $(body).click(
+//     console.log('it works...')
+//   );
+// }
+//
+// testpage();
+
 if(require.main === module) {
     app.listen(process.env.PORT || 4747, function() {
       console.info(`App is listening on ${this.address().port}`);
