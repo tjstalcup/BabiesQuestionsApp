1bb55c8400303b4a84405c4f41f15eade0422cac
TJ Stalcup <tstalcupjr@gmail.com>
2019-01-15 22:13:01 -0500

    adds .env configuration

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 "use strict";
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/questionsboard';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/questionsboard'; //mlab connection string here
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-questionsboard';
 exports.PORT = process.env.PORT || 4747;
+// two options here. either put your mlab connection string on line 2
+// OR - use dotenv package, which I will illustrate now.
+// can you provide me your mlab connection string?
--- mainServer.js
+++ mainServer.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+require('dotenv').config();
 // const bodyParser = require("body-parser");
 
 mongoose.Promise = global.Promise;
